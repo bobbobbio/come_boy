eae90ac25dbe9ebf317204b45bde35844849bc8b
Remi Bernotavicius <remi@kunugiken.com>
2016-09-10 19:45:51 -0700

    Add an end-to-end test for the opcode genration code.

--- src/emulator_lr35902/emulator_8080/opcodes/opcode_gen.py
+++ src/emulator_lr35902/emulator_8080/opcodes/opcode_gen.py
@@ -29,6 +29,153 @@ def name_args(args):
     return ['{}{}: {}'.format(arg[0], num + 1, arg[1])
         for num, arg in enumerate(args)]
 
+def generate(opcode_dict, out_file):
+    out_file.write(textwrap.dedent('''
+        use emulator_lr35902::emulator_{0}::opcodes::{{
+            read_u16, read_u8, Register{0}, OpcodePrinter{0}}};
+
+        /*
+         * Warning: This file is generated.  Don't manually edit.
+         * Instead edit opcodes/opcode_gen.py
+         */
+    '''.format(INSTRUCTION_SET_NAME)))
+
+    #   __                  _   _               _        _     _
+    #  / _|_   _ _ __   ___| |_(_) ___  _ __   | |_ __ _| |__ | | ___
+    # | |_| | | | '_ \ / __| __| |/ _ \| '_ \  | __/ _` | '_ \| |/ _ \
+    # |  _| |_| | | | | (__| |_| | (_) | | | | | || (_| | |_) | |  __/
+    # |_|  \__,_|_| |_|\___|\__|_|\___/|_| |_|  \__\__,_|_.__/|_|\___|
+    #
+
+    functions = {}
+    for info in opcode_dict.itervalues():
+        instr = info['instr']
+        if instr == '-':
+            name = 'not_implemented'
+        else:
+            name = info['description'].replace(' ', '_').lower()
+
+        arg_desc = []
+        for arg in info['args']:
+            if arg == 'adr':
+                arg_desc.append(('address', 'u16'))
+            elif arg.startswith('D') and arg != 'D':
+                arg_desc.append(('data', 'u' + arg[1:]))
+            elif all([c.isdigit() for c in arg]):
+                arg_desc.append(('implicit_data', 'u8'))
+            else:
+                assert all([c.isalpha() for c in arg])
+                arg_desc.append(('register',
+                    'Register{}'.format(INSTRUCTION_SET_NAME)))
+
+        if name not in functions:
+            functions[name] = instr, arg_desc
+        else:
+            _, existing_arg_desc = functions[name]
+            assert existing_arg_desc == arg_desc, \
+                "{} has non consistant arugments".format(name)
+
+    #  _           _                   _   _
+    # (_)_ __  ___| |_ _ __ _   _  ___| |_(_) ___  _ __  ___
+    # | | '_ \/ __| __| '__| | | |/ __| __| |/ _ \| '_ \/ __|
+    # | | | | \__ \ |_| |  | |_| | (__| |_| | (_) | | | \__ \
+    # |_|_| |_|___/\__|_|   \__,_|\___|\__|_|\___/|_| |_|___/
+    #  _             _ _
+    # | |_ _ __ __ _(_) |_
+    # | __| '__/ _` | | __|
+    # | |_| | | (_| | | |_
+    #  \__|_|  \__,_|_|\__|
+
+    out_file.write(textwrap.dedent('''
+      pub trait InstructionSet{} {{
+    '''.format(INSTRUCTION_SET_NAME)))
+
+    for name, info in functions.iteritems():
+        _, args = info
+        out_file.write('    '
+            'fn {}({});\n'.format(
+                name.lower(),
+                ', '.join(['&mut self'] + name_args(args))))
+
+    out_file.write('}\n')
+
+    out_file.write(textwrap.dedent('''
+        pub fn dispatch_{0}_opcode<I: InstructionSet{0}>(
+            mut stream: &[u8],
+            machine: &mut I)
+        {{
+            match read_u8(&mut stream).ok().expect("") {{
+    '''.format(INSTRUCTION_SET_NAME)))
+
+    for opcode, info in opcode_dict.iteritems():
+        out_file.write('        {} => '.format(opcode))
+        instr = info['instr']
+        if instr == '-':
+            name = 'not_implemented'
+        else:
+            name = info['description'].replace(' ', '_').lower()
+        out_file.write('machine.{}({}),\n'.format(
+            name, ', '.join(read_args(info['args'], '&mut stream'))))
+
+    out_file.write(textwrap.dedent('''
+               _ => panic!("Unknown opcode")
+          };
+       }
+    '''))
+
+    out_file.write(textwrap.dedent('''
+        pub fn get_{}_opcode_size(opcode: u8) -> u8
+        {{
+            match opcode {{
+    '''.format(INSTRUCTION_SET_NAME)))
+
+    for opcode, info in opcode_dict.iteritems():
+        out_file.write('        {} => {},\n'.format(opcode, info['size']))
+
+    out_file.write(textwrap.dedent('''
+               _ => panic!("Unknown opcode")
+          }
+       }
+    '''))
+
+    #                            _                   _       _
+    #   ___  _ __   ___ ___   __| | ___   _ __  _ __(_)_ __ | |_ ___ _ __
+    #  / _ \| '_ \ / __/ _ \ / _` |/ _ \ | '_ \| '__| | '_ \| __/ _ \ '__|
+    # | (_) | |_) | (_| (_) | (_| |  __/ | |_) | |  | | | | | ||  __/ |
+    #  \___/| .__/ \___\___/ \__,_|\___| | .__/|_|  |_|_| |_|\__\___|_|
+    #       |_|                          |_|
+
+    out_file.write(textwrap.dedent('''
+        impl<'a> InstructionSet{0} for OpcodePrinter{0}<'a> {{
+    '''.format(INSTRUCTION_SET_NAME)))
+
+    for name, info in functions.iteritems():
+        instr, args = info
+        out_file.write('    '
+            'fn {}({})\n    {{\n'.format(
+                name.lower(), ', '.join(['&mut self'] + name_args(args))))
+        def print_to_out_file(pattern, arg_name):
+            out_file.write('        '
+                'write!(self.stream_out, '
+                '"{}", {}).ok().expect("");\n'.format(
+                    pattern, arg_name))
+
+        print_to_out_file("{:04}", '"{}"'.format(instr))
+
+        for arg, arg_decl in zip(args, name_args(args)):
+            arg_type = arg[0]
+            arg_name = arg_decl.split(':')[0]
+            if arg_type == 'data':
+                print_to_out_file(" #${:02x}", arg_name)
+            elif arg_type == 'implicit_data':
+                print_to_out_file(" {}", arg_name)
+            elif arg_type == 'address':
+                print_to_out_file(" ${:02x}", arg_name)
+            else:
+                print_to_out_file(" {:?}", arg_name)
+        out_file.write('    }\n')
+    out_file.write('}')
+
 def main():
     script_dir = os.path.dirname(os.path.realpath(__file__))
 
@@ -37,151 +184,7 @@ def main():
     output_file = os.path.join(script_dir, 'opcode_gen.rs')
 
     with open(output_file, 'w') as out_file:
-        out_file.write(textwrap.dedent('''
-            use emulator_lr35902::emulator_{0}::opcodes::{{
-                read_u16, read_u8, Register{0}, OpcodePrinter{0}}};
-
-            /*
-             * Warning: This file is generated.  Don't manually edit.
-             * Instead edit opcodes/opcode_gen.py
-             */
-        '''.format(INSTRUCTION_SET_NAME)))
-
-        #   __                  _   _               _        _     _
-        #  / _|_   _ _ __   ___| |_(_) ___  _ __   | |_ __ _| |__ | | ___
-        # | |_| | | | '_ \ / __| __| |/ _ \| '_ \  | __/ _` | '_ \| |/ _ \
-        # |  _| |_| | | | | (__| |_| | (_) | | | | | || (_| | |_) | |  __/
-        # |_|  \__,_|_| |_|\___|\__|_|\___/|_| |_|  \__\__,_|_.__/|_|\___|
-        #
-
-        functions = {}
-        for info in opcode_dict.itervalues():
-            instr = info['instr']
-            if instr == '-':
-                name = 'not_implemented'
-            else:
-                name = info['desciption'].replace(' ', '_').lower()
-
-            arg_desc = []
-            for arg in info['args']:
-                if arg == 'adr':
-                    arg_desc.append(('address', 'u16'))
-                elif arg.startswith('D') and arg != 'D':
-                    arg_desc.append(('data', 'u' + arg[1:]))
-                elif all([c.isdigit() for c in arg]):
-                    arg_desc.append(('implicit_data', 'u8'))
-                else:
-                    assert all([c.isalpha() for c in arg])
-                    arg_desc.append(('register',
-                        'Register{}'.format(INSTRUCTION_SET_NAME)))
-
-            if name not in functions:
-                functions[name] = instr, arg_desc
-            else:
-                _, existing_arg_desc = functions[name]
-                assert existing_arg_desc == arg_desc, \
-                    "{} has non consistant arugments".format(name)
-
-        #  _           _                   _   _
-        # (_)_ __  ___| |_ _ __ _   _  ___| |_(_) ___  _ __  ___
-        # | | '_ \/ __| __| '__| | | |/ __| __| |/ _ \| '_ \/ __|
-        # | | | | \__ \ |_| |  | |_| | (__| |_| | (_) | | | \__ \
-        # |_|_| |_|___/\__|_|   \__,_|\___|\__|_|\___/|_| |_|___/
-        #  _             _ _
-        # | |_ _ __ __ _(_) |_
-        # | __| '__/ _` | | __|
-        # | |_| | | (_| | | |_
-        #  \__|_|  \__,_|_|\__|
-
-        out_file.write(textwrap.dedent('''
-          pub trait InstructionSet{} {{
-        '''.format(INSTRUCTION_SET_NAME)))
-
-        for name, info in functions.iteritems():
-            _, args = info
-            out_file.write('    '
-                'fn {}({});\n'.format(
-                    name.lower(),
-                    ', '.join(['&mut self'] + name_args(args))))
-
-        out_file.write('}\n')
-
-        out_file.write(textwrap.dedent('''
-            pub fn dispatch_{0}_opcode<I: InstructionSet{0}>(
-                mut stream: &[u8],
-                machine: &mut I)
-            {{
-                match read_u8(&mut stream).ok().expect("") {{
-        '''.format(INSTRUCTION_SET_NAME)))
-
-        for opcode, info in opcode_dict.iteritems():
-            out_file.write('        {} => '.format(opcode))
-            instr = info['instr']
-            if instr == '-':
-                name = 'not_implemented'
-            else:
-                name = info['desciption'].replace(' ', '_').lower()
-            out_file.write('machine.{}({}),\n'.format(
-                name, ', '.join(read_args(info['args'], '&mut stream'))))
-
-        out_file.write(textwrap.dedent('''
-                   _ => panic!("Unknown opcode")
-              };
-           }
-        '''))
-
-        out_file.write(textwrap.dedent('''
-            pub fn get_{}_opcode_size(opcode: u8) -> u8
-            {{
-                match opcode {{
-        '''.format(INSTRUCTION_SET_NAME)))
-
-        for opcode, info in opcode_dict.iteritems():
-            out_file.write('        {} => {},\n'.format(opcode, info['size']))
-
-        out_file.write(textwrap.dedent('''
-                   _ => panic!("Unknown opcode")
-              }
-           }
-        '''))
-
-        #                            _                   _       _
-        #   ___  _ __   ___ ___   __| | ___   _ __  _ __(_)_ __ | |_ ___ _ __
-        #  / _ \| '_ \ / __/ _ \ / _` |/ _ \ | '_ \| '__| | '_ \| __/ _ \ '__|
-        # | (_) | |_) | (_| (_) | (_| |  __/ | |_) | |  | | | | | ||  __/ |
-        #  \___/| .__/ \___\___/ \__,_|\___| | .__/|_|  |_|_| |_|\__\___|_|
-        #       |_|                          |_|
-
-        out_file.write(textwrap.dedent('''
-            impl<'a> InstructionSet{0} for OpcodePrinter{0}<'a> {{
-        '''.format(INSTRUCTION_SET_NAME)))
-
-        for name, info in functions.iteritems():
-            instr, args = info
-            out_file.write('    '
-                'fn {}({})\n    {{\n'.format(
-                    name.lower(), ', '.join(['&mut self'] + name_args(args))))
-            def print_to_out_file(pattern, arg_name):
-                out_file.write('        '
-                    'write!(self.stream_out, '
-                    '"{}", {}).ok().expect("{}");\n'.format(
-                        pattern, arg_name, 'Failed to Write to Stream'))
-
-            print_to_out_file("{:04}", '"{}"'.format(instr))
-
-            for arg, arg_decl in zip(args, name_args(args)):
-                arg_type = arg[0]
-                arg_name = arg_decl.split(':')[0]
-                if arg_type == 'data':
-                    print_to_out_file(" #${:02x}", arg_name)
-                elif arg_type == 'implicit_data':
-                    print_to_out_file(" {}", arg_name)
-                elif arg_type == 'address':
-                    print_to_out_file(" ${:02x}", arg_name)
-                else:
-                    print_to_out_file(" {:?}", arg_name)
-            out_file.write('    }\n')
-        out_file.write('}')
+        generate(opcode_dict, out_file)
 
 if __name__ == "__main__":
     main()
--- src/emulator_lr35902/emulator_8080/opcodes/opcode_gen_test.py
+++ src/emulator_lr35902/emulator_8080/opcodes/opcode_gen_test.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
+import StringIO
+import textwrap
 import unittest
+
 import opcode_gen
 
 class OpcodeGenTest(unittest.TestCase):
@@ -21,6 +24,69 @@ class OpcodeGenTest(unittest.TestCase):
     def test_read_args_with_multiple(self):
         self.assertEqual(opcode_gen.read_args(['A', '12'], None),
             ['Register8080::A', '12 as u8'])
+
+class OpcodeGenFullTest(unittest.TestCase):
+    def setUp(self):
+        self.outfile = StringIO.StringIO()
+
+    def test_opcodes_no_args(self):
+        opcode_dict = {
+            "0x00": {"instr": "HELLO",
+                     "description": "print hello",
+                     "args": [],
+                     "size": 1},
+            "0x01": {"instr": "GOODBYE",
+                     "description": "print goodbye",
+                     "args": [],
+                     "size": 1}
+        }
+        opcode_gen.generate(opcode_dict, self.outfile)
+        self.assertEqual(self.outfile.getvalue(), textwrap.dedent('''
+        use emulator_lr35902::emulator_8080::opcodes::{
+            read_u16, read_u8, Register8080, OpcodePrinter8080};
+
+        /*
+         * Warning: This file is generated.  Don't manually edit.
+         * Instead edit opcodes/opcode_gen.py
+         */
+
+        pub trait InstructionSet8080 {
+            fn print_hello(&mut self);
+            fn print_goodbye(&mut self);
+        }
+
+        pub fn dispatch_8080_opcode<I: InstructionSet8080>(
+            mut stream: &[u8],
+            machine: &mut I)
+        {
+            match read_u8(&mut stream).ok().expect("") {
+                0x00 => machine.print_hello(),
+                0x01 => machine.print_goodbye(),
+
+                _ => panic!("Unknown opcode")
+           };
+        }
+
+        pub fn get_8080_opcode_size(opcode: u8) -> u8
+        {
+            match opcode {
+                0x00 => 1,
+                0x01 => 1,
+
+                _ => panic!("Unknown opcode")
+           }
+        }
+
+        impl<'a> InstructionSet8080 for OpcodePrinter8080<'a> {
+            fn print_hello(&mut self)
+            {
+                write!(self.stream_out, "{:04}", "HELLO").ok().expect("");
+            }
+            fn print_goodbye(&mut self)
+            {
+                write!(self.stream_out, "{:04}", "GOODBYE").ok().expect("");
+            }
+        }'''))
 
 if __name__ == "__main__":
     unittest.main()
