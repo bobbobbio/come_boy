55caee9c92f87ebac8f9b9839d27855759abab4a
Remi Bernotavicius <remi@kunugiken.com>
2017-07-01 22:36:10 -0700

    Add a hash function for later use.

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -3,15 +3,15 @@
 use std::io::{self, Result};
 use std::mem;
 
+#[cfg(test)]
+use std::slice;
+
 pub fn read_u16<T: io::Read>(
     mut stream: T) -> Result<u16>
 {
-    let narg : u16;
     let mut arg_buffer = [0; 2];
     try!(stream.read_exact(&mut arg_buffer));
-    unsafe {
-        narg = mem::transmute(arg_buffer);
-    }
+    let narg: u16 = unsafe { mem::transmute(arg_buffer) };
     Ok(u16::from_le(narg))
 }
 
@@ -61,4 +61,112 @@ fn twos_complement_u8()
 fn twos_complement_u16()
 {
     assert_eq!(0b0111000000001010u16.twos_complement(), 0b1000111111110110u16);
+}
+
+#[cfg(test)]
+fn get_16_bits(data: &[u8], index: usize) -> u16
+{
+    (data[index + 1] as u16) << 8 | data[index] as u16
+}
+
+// This was taken from http://www.azillionmonkeys.com/qed/hash.html. The code
+// for this function (and only this function) below is LGPL 2.1. I have ported it to rust, but
+// kept the same behavior.
+#[cfg(test)]
+fn super_fast_hash_iter<T: Sized>(data_in: &T, mut hash: u32) -> u32
+{
+    let mut len = mem::size_of_val(data_in);
+    let data: &[u8] = unsafe { mem::transmute(slice::from_raw_parts(data_in, len)) };
+
+    let mut tmp: u32;
+    let mut i = 0;
+
+    let rem = (len & 3) as u32;
+    len >>= 2;
+
+    /* Main loop */
+    for _ in 0..len {
+        hash = hash.wrapping_add(get_16_bits(data, i) as u32);
+        tmp    = ((get_16_bits(data, i + 2) as u32) << 11) ^ hash;
+        hash   = (hash << 16) ^ tmp;
+        i     += 4;
+        hash  = hash.wrapping_add(hash >> 11);
+    }
+
+    /* Handle end cases */
+    match rem {
+        3 => {
+            hash = hash.wrapping_add(get_16_bits(data, i) as u32);
+            hash ^= hash << 16;
+            hash ^= ((data[i + 2] as i8) as u32) << 18;
+            hash = hash.wrapping_add(hash >> 11);
+        },
+
+        2 => {
+            hash = hash.wrapping_add(get_16_bits(data, i) as u32);
+            hash ^= hash << 11;
+            hash = hash.wrapping_add(hash >> 17);
+        },
+
+        1 => {
+            hash = hash.wrapping_add((data[i] as i8) as u32);
+            hash ^= hash << 10;
+            hash = hash.wrapping_add(hash >> 1);
+        },
+
+        _ => { },
+    }
+
+    /* Force "avalanching" of final 127 bits */
+    hash ^= hash << 3;
+    hash = hash.wrapping_add(hash >> 5);
+    hash ^= hash << 4;
+    hash = hash.wrapping_add(hash >> 17);
+    hash ^= hash << 25;
+    hash = hash.wrapping_add(hash >> 6);
+
+    return hash;
+}
+
+#[cfg(test)]
+fn super_fast_hash<T: Sized>(data_in: &T) -> u32
+{
+    let len = mem::size_of_val(data_in);
+    super_fast_hash_iter(data_in, len as u32)
+}
+
+// These values were taken by running the original C version.
+#[test]
+fn super_fast_hash_example_1()
+{
+    let v = [0x88u8, 0x99u8, 0x10u8, 0x11u8, 0x09u8];
+    assert_eq!(super_fast_hash(&v), 284656667);
+}
+
+#[test]
+fn super_fast_hash_example_2()
+{
+    let v = [0x77u8, 0x01u8, 0x12u8, 0x24u8];
+    assert_eq!(super_fast_hash(&v), 700799581);
+}
+
+#[test]
+fn super_fast_hash_example_3()
+{
+    let v = [0x91u8, 0x00u8, 0x84u8];
+    assert_eq!(super_fast_hash(&v), 505819445);
+}
+
+#[test]
+fn super_fast_hash_example_4()
+{
+    let v = [0x11u8, 0x05u8];
+    assert_eq!(super_fast_hash(&v), 3238191665);
+}
+
+#[test]
+fn super_fast_hash_example_5()
+{
+    let v = [0x45u8];
+    assert_eq!(super_fast_hash(&v), 3114100952);
 }
