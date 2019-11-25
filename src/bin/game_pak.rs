32c739118cd649a23c72d2d331f42c5814d11582
Remi Bernotavicius <remi@kunugiken.com>
2019-11-25 00:58:05 -0800

    Add a CLI tool for printing GamePak information

---
+++ src/bin/game_pak.rs
@@ -0,0 +1,21 @@
+// Copyright 2019 Remi Bernotavicius
+
+use come_boy::game_boy_emulator::{GamePak, Result};
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(name = "GamePak Info", about = "Prints information about GamePaks")]
+struct Options {
+    #[structopt(parse(from_os_str))]
+    rom: PathBuf,
+}
+
+fn main() -> Result<()> {
+    let options = Options::from_args();
+
+    let game_pak = GamePak::from_path(options.rom)?;
+    println!("{:?}", game_pak);
+
+    Ok(())
+}
