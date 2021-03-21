42dea904a9c94276e3eebe13f1a6e3f43321458a
Remi Bernotavicius <remi@abort.cc>
2021-03-20 19:20:01 -0700

    Attempt to fix debugger build on Windows

--- src/bin/debugger.rs
+++ src/bin/debugger.rs
@@ -4,20 +4,19 @@ use bin_common::backend::BackendMap;
 use bin_common::Result;
 use come_boy::game_boy_emulator::{self, GamePak};
 use come_boy::rendering::{Renderer, RenderingOptions};
-use nix::sys::signal;
 use std::path::PathBuf;
 use std::sync::atomic::{AtomicBool, Ordering};
 use structopt::StructOpt;
+
+#[cfg(any(target_os = "linux", target_os = "macos"))]
+use nix::sys::signal;
 
 #[path = "../bin_common/mod.rs"]
 mod bin_common;
 
 static INTERRUPTED: AtomicBool = AtomicBool::new(false);
 
-extern "C" fn handle_sigint(_: i32) {
-    INTERRUPTED.store(true, Ordering::Relaxed)
-}
-
+#[allow(unused)]
 #[derive(StructOpt)]
 #[structopt(name = "Come Boy Debugger", about = "Game Boy (DMG) emulator debugger")]
 struct Options {
@@ -49,13 +48,23 @@ impl bin_common::frontend::Frontend for Frontend {
     }
 }
 
-fn main() -> Result<()> {
+#[cfg(any(target_os = "linux", target_os = "macos"))]
+fn set_up_signal_handler() {
+    extern "C" fn handle_sigint(_: i32) {
+        INTERRUPTED.store(true, Ordering::Relaxed)
+    }
+
     let sig_action = signal::SigAction::new(
         signal::SigHandler::Handler(handle_sigint),
         signal::SaFlags::empty(),
         signal::SigSet::empty(),
     );
     unsafe { signal::sigaction(signal::SIGINT, &sig_action) }.unwrap();
+}
+
+fn main() -> Result<()> {
+    #[cfg(any(target_os = "linux", target_os = "macos"))]
+    set_up_signal_handler();
 
     let options = Options::from_args();
 
