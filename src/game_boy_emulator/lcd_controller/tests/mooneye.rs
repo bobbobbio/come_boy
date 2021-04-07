681dfc19b3d47b6a48bf48d9508e2aee811a1c7f
Remi Bernotavicius <remi@abort.cc>
2021-04-06 17:33:53 -0700

    Look up stop address in sym file for mooneye tests

--- src/game_boy_emulator/lcd_controller/tests/mooneye.rs
+++ src/game_boy_emulator/lcd_controller/tests/mooneye.rs
@@ -6,25 +6,25 @@ use crate::game_boy_emulator::tests::mooneye::run_mooneye_test_rom;
 /// bits, but they are still writable and readable normally"
 #[test]
 fn mooneye_test_rom_acceptance_bits_mem_oam() {
-    run_mooneye_test_rom("acceptance/bits/mem_oam.gb", 0x486e);
+    run_mooneye_test_rom("acceptance/bits/mem_oam.gb");
 }
 
 /// "This test checks that OAM DMA copies all bytes correctly"
 #[test]
 fn mooneye_test_rom_acceptance_oam_dma_basic() {
-    run_mooneye_test_rom("acceptance/oam_dma/basic.gb", 0x486e);
+    run_mooneye_test_rom("acceptance/oam_dma/basic.gb");
 }
 
 /// "This test checks what happens if you read the DMA register. Reads should always simply return
 /// the last written value, regardless of the state of the OAM DMA transfer or other things"
 #[test]
 fn mooneye_test_rom_acceptance_oam_dma_reg_read() {
-    run_mooneye_test_rom("acceptance/oam_dma/reg_read.gb", 0x486e);
+    run_mooneye_test_rom("acceptance/oam_dma/reg_read.gb");
 }
 
 /// "This test checks that OAM DMA source memory areas work as expected, including the area past
 /// $DFFF"
 #[test]
 fn mooneye_test_rom_acceptance_oam_dma_sources_gs() {
-    run_mooneye_test_rom("acceptance/oam_dma/sources-GS.gb", 0x490e);
+    run_mooneye_test_rom("acceptance/oam_dma/sources-GS.gb");
 }
--- src/game_boy_emulator/tests/mooneye.rs
+++ src/game_boy_emulator/tests/mooneye.rs
@@ -4,6 +4,7 @@ use super::run_emulator_until_pc;
 use crate::emulator_common::disassembler::MemoryAccessor;
 use crate::game_boy_emulator::{memory_controller::GameBoyMemoryMap, GameBoyEmulator, GamePak};
 use crate::lr35902_emulator::tests::{read_screen_message, read_test_rom};
+use std::{fs, path::PathBuf};
 
 fn assert_mooneye_test_rom_success<M: MemoryAccessor>(memory_accessor: &M) {
     let message = read_screen_message(memory_accessor);
@@ -27,7 +28,27 @@ fn read_mooneye_test_rom(name: &str) -> Vec<u8> {
     read_test_rom("mooneye_test_roms", name)
 }
 
-pub(crate) fn run_mooneye_test_rom(rom_path: &str, stop_address: u16) {
+// XXX: Should replace with fully fleshed symbol file parsing eventually.
+fn read_mooneye_stop_address(rom_path: &str) -> u16 {
+    let rom_path = PathBuf::from(format!("mooneye_test_roms/{}", rom_path));
+    let stem = rom_path.file_stem().unwrap().to_str().unwrap().to_owned();
+    let sym_path = rom_path.with_file_name(stem + ".sym");
+
+    println!("sym-path {:?}", sym_path);
+    let contents = fs::read_to_string(sym_path).unwrap();
+    let sym_line = contents
+        .lines()
+        .filter(|l| l.contains("quit@halt_execution_0"))
+        .next()
+        .unwrap();
+
+    let location = sym_line.split(" ").next().unwrap();
+    let address = location.split(":").skip(1).next().unwrap();
+    u16::from_str_radix(address, 16).unwrap()
+}
+
+pub(crate) fn run_mooneye_test_rom(rom_path: &str) {
+    let stop_address = read_mooneye_stop_address(rom_path);
     let mut e = GameBoyEmulator::new();
     e.load_game_pak(GamePak::new(&read_mooneye_test_rom(rom_path), None));
     run_emulator_until_pc(&mut e, stop_address);
@@ -37,25 +58,25 @@ pub(crate) fn run_mooneye_test_rom(rom_path: &str, stop_address: u16) {
 /// "Tests the DAA instruction with all possible input combinations"
 #[test]
 fn mooneye_test_rom_acceptance_instr_daa() {
-    run_mooneye_test_rom("acceptance/instr/daa.gb", 0x686e);
+    run_mooneye_test_rom("acceptance/instr/daa.gb");
 }
 
 /// "This test checks that bottom 4 bits of the F register always return 0"
 #[test]
 fn mooneye_test_rom_acceptance_bits_reg_f() {
-    run_mooneye_test_rom("acceptance/bits/reg_f.gb", 0x4b2e);
+    run_mooneye_test_rom("acceptance/bits/reg_f.gb");
 }
 
 /// "This test checks all unused bits in working $FFxx IO, and all unused $FFxx IO. Unused bits and
 /// unused IO all return 1s"
 #[test]
 fn mooneye_test_rom_acceptance_bits_unused_hwio_gs() {
-    run_mooneye_test_rom("acceptance/bits/unused_hwio-GS.gb", 0x486e);
+    run_mooneye_test_rom("acceptance/bits/unused_hwio-GS.gb");
 }
 
 /// "Tests what happens if the IE register is the target for one of the PC pushes during interrupt
 /// dispatch."
 #[test]
 fn mooneye_test_rom_acceptance_interrupts_ie_push() {
-    run_mooneye_test_rom("acceptance/interrupts/ie_push.gb", 0x486e);
+    run_mooneye_test_rom("acceptance/interrupts/ie_push.gb");
 }
