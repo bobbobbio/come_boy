31ed558ccbab85d2ddf591f5011f6c4e5b417a53
Remi Bernotavicius <remi@abort.cc>
2021-04-09 01:54:51 -0700

    Add HTML/JS glue for web

--- web/src/lib.rs
+++ web/src/lib.rs
@@ -1,7 +1,19 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+#[wasm_bindgen(start)]
+pub fn start() -> Result<(), JsValue> {
+    console_error_panic_hook::set_once();
+    console_log!("Come Boy Starting");
+
+    Ok(())
 }
---
+++ web/www/main.js
@@ -0,0 +1,1 @@
+import * as wasm from "come_boy_web";
