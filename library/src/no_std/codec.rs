b038052bb00be2e7041da551cfde4235446c12b9
Remi Bernotavicius <remi@abort.cc>
2021-11-08 18:39:59 -0800

    no_std: Add a codec implementation for use with no_std

---
+++ library/src/no_std/codec.rs
@@ -0,0 +1,27 @@
+// copyright 2021 Remi Bernotavicius
+
+#![cfg_attr(feature = "std", allow(dead_code))]
+
+//! This module is here so no_std will compile.
+//! I need some serialization format that supports no_std
+
+use super::io::{self, Result};
+use serde::{de::DeserializeOwned, Serialize};
+
+pub struct Error;
+
+pub fn serialize_into<W, T: ?Sized>(_writer: W, _value: &T) -> Result<()>
+where
+    W: io::Write,
+    T: Serialize,
+{
+    unimplemented!()
+}
+
+pub fn deserialize_from<R, T>(_reader: R) -> Result<T>
+where
+    R: io::Read,
+    T: DeserializeOwned,
+{
+    unimplemented!()
+}
--- library/src/no_std/mod.rs
+++ library/src/no_std/mod.rs
@@ -1,2 +1,3 @@
 // copyright 2021 Remi Bernotavicius
+pub mod codec;
 pub mod io;
